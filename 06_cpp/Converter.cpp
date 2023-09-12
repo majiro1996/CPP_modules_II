5ef4f3374ec279f32cc1751e5e1051b3ad535808
majiro1996 <majiro1996@gmail.com>
2023-09-12 15:37:37 +0200

    wip

--- 06_cpp/Converter.cpp
+++ 06_cpp/Converter.cpp
@@ -6,11 +6,16 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:12:52 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/11 17:02:58 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:37:01 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Converter.hpp"
+
+int  ScalarConverter::myStoi(std::string str)
+{
+
+}
 
 bool	ScalarConverter::_isChar(std::string str)
 {
@@ -19,33 +24,20 @@ bool	ScalarConverter::_isChar(std::string str)
 	return (false);
 }
 
-//To check if a string is a float or double, use std::stof() and std::stod()
-//they will throw an exception if the string is not the correct type
-
 bool	ScalarConverter::_isInt(std::string str)
 {
-	try
-	{
-		std::stoi(str);
-	}
-	catch (std::exception &e)
-	{
+	const char	*ptr = str.c_str();
+	char		*endptr;
+	std::strtol(ptr, &endptr, 10);
+
+	if (*endptr != '\0')
 		return (false);
-	}
 	return (true);
 }
 
 bool	ScalarConverter::_isFloat(std::string str)
 {
-	try
-	{
-		std::stof(str);
-	}
-	catch (std::exception &e)
-	{
-		return (false);
-	}
-	return (true);
+	
 }
 
 bool	ScalarConverter::_isDouble(std::string str)
@@ -61,9 +53,60 @@ bool	ScalarConverter::_isDouble(std::string str)
 	return (true);
 }
 
+void	ScalarConverter::_convertChar(std::string str)
+{
+	std::cout << "char: '" << str[0] << "'" << std::endl;
+	char	c = str[0];
 
+	std::cout << "char: '" << c << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+}
 
-void ScalarConverter::_convert(std::string str)
+void	ScalarConverter::_convertInt(std::string str)
+{
+	std::cout << "int: " << str << std::endl;
+	int		i = std::stoi(str);
+
+	if (i < 32 || i > 126)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(i) << "'" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+}
+
+void	ScalarConverter::_convertFloat(std::string str)
+{
+	std::cout << "float: " << str << "f" << std::endl;
+	float	f = std::stof(str);
+
+	if (f < 32 || f > 126)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(f) << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(f) << std::endl;
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << static_cast<double>(f) << std::endl;
+}
+
+void	ScalarConverter::_convertDouble(std::string str)
+{
+	std::cout << "double: " << str << std::endl;
+	double	d = std::stod(str);
+
+	if (d < 32 || d > 126)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(d) << std::endl;
+	std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
+}
+
+void ScalarConverter::convert(std::string str)
 {
 	if (_isChar(str))
 		_convertChar(str);
--- 06_cpp/Converter.hpp
+++ 06_cpp/Converter.hpp
@@ -6,23 +6,29 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:07:01 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/11 10:41:14 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/12 13:16:47 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONVERTER_HPP
 # define CONVERTER_HPP
 
-# include <iostream>
-# include <string>
-# include <cmath>
-# include <limits>
-# include <iomanip>
-# include <sstream>
+# include <iostream> // std::cout
+# include <string> // std::string
+# include <cmath> // std::isnan
+# include <limits> // std::numeric_limits
+# include <iomanip> // std::setprecision
+# include <sstream> // std::stringstream
 
 class ScalarConverter
 {
 	private:
+
+		static int myStoi(std::string str);
+		static int myStof(std::string str);
+		static int myStod(std::string str);
+		static int myStold(std::string str);
+
 		static bool	_isChar(std::string str);
 		static bool	_isInt(std::string str);
 		static bool	_isFloat(std::string str);
@@ -32,6 +38,15 @@ class ScalarConverter
 		static void	_convertInt(std::string str);
 		static void	_convertFloat(std::string str);
 		static void	_convertDouble(std::string str);
+
+		class NotConvertibleException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("impossible");
+				}
+		};
 
 	public:
 		static void	convert(std::string str);

323ba990046d64b31f7defb097608f725ee38d05
Majiro <majiro1996@gmail.com>
2023-09-19 01:06:34 +0200

    07-00

--- 07_cpp/ex00/includes/Templates.hpp
+++ 07_cpp/ex00/includes/Templates.hpp
@@ -1,16 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inc.hpp                                            :+:      :+:    :+:   */
+/*   Templates.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 09:07:01 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/18 23:45:10 by manujime         ###   ########.fr       */
+/*   Created: 2023/09/19 00:50:00 by manujime          #+#    #+#             */
+/*   Updated: 2023/09/19 00:50:14 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef 
-# define 
+#ifndef TEMPLATES_HPP
+# define TEMPLATES_HPP
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+    T	tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T	min(T a, T b)
+{
+    if (a < b)
+        return (a);
+    return (b);
+}
+
+template <typename T>
+T	max(T a, T b)
+{
+    if (a > b)
+        return (a);
+    return (b);
+}
 
 #endif
--- 07_cpp/ex00/sources/main.cpp
+++ 07_cpp/ex00/sources/main.cpp
@@ -6,13 +6,33 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:44:06 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/18 23:45:29 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/19 01:01:24 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "../includes/Templates.hpp"
+#include <iostream>
 
-int	main(int argc, char **argv)
+
+int	main(void)
 {
+    int a = 4;
+    int b = 2;
+    std::cout << "a: " << a << " b: " << b << std::endl;
+    swap(a, b);
+    std::cout << "a: " << a << " b: " << b << std::endl;
+    std::cout << "min: " << ::min(a, b) << std::endl;
+    std::cout << "max: " << ::max(a, b) << std::endl;
+    std::cout << std::endl;
 
+    std::string c = "something";
+    std::string d = "else";
+    std::cout << "c: " << c << " d: " << d << std::endl;
+    swap(c, d);
+    std::cout << "c: " << c << " d: " << d << std::endl;
+    std::cout << "min: " << ::min(c, d) << std::endl;
+    std::cout << "max: " << ::max(c, d) << std::endl;
+    std::cout << std::endl;
+    
+    return (0);
 }

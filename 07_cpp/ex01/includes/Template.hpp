c6c082a324878d0d8907b55ae72af332bde8e34d
Majiro <majiro1996@gmail.com>
2023-09-19 12:29:07 +0200

    07-01 iter

--- 07_cpp/ex01/includes/Template.hpp
+++ 07_cpp/ex01/includes/Template.hpp
@@ -1,16 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inc.hpp                                            :+:      :+:    :+:   */
+/*   Template.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 09:07:01 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/18 23:45:10 by manujime         ###   ########.fr       */
+/*   Created: 2023/09/19 10:58:08 by manujime          #+#    #+#             */
+/*   Updated: 2023/09/19 11:21:08 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef 
-# define 
+#ifndef TEMPLATE_HPP
+# define TEMPLATE_HPP
+
+# include <iostream>
+
+template <typename T>
+void   testPrint(T &a)
+{
+    std::cout << a << std::endl;
+}
+
+template <typename T>
+void   iter(T *a, int len, void (*f)(T &))
+{
+    for (int i = 0; i < len; i++)
+        f(a[i]);
+}
 
 #endif
--- 07_cpp/ex01/sources/main.cpp
+++ 07_cpp/ex01/sources/main.cpp
@@ -6,13 +6,23 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:44:06 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/18 23:45:29 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/19 12:26:21 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "../includes/Template.hpp"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
+    int     a[5] = {1, 2, 3, 4, 5};
 
+    iter(a, 5, testPrint);
+
+    std::cout << std::endl;
+
+    std::string b[5] = {"one", "two", "three", "four", "five"};
+
+    iter(b, 5, testPrint);
+
+    return (0);
 }

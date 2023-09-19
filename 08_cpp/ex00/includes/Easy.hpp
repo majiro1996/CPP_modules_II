65192fe2fa3b3c72197675bdade23587a19e6d4b
Majiro <majiro1996@gmail.com>
2023-09-19 18:42:42 +0200

    08-00 basic test

--- 08_cpp/ex00/includes/Easy.hpp
+++ 08_cpp/ex00/includes/Easy.hpp
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Inc.hpp                                            :+:      :+:    :+:   */
+/*   Easy.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 00:50:00 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/19 18:20:54 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/19 18:39:24 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef INC_HPP
-# define INC_HPP
+#ifndef EASY_HPP
+# define EASY_HPP
+
+# include <iostream>
+# include <string>
+# include <algorithm>
+
+template <typename T>
+bool  easyfind(T &container, int n)
+{
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), n);
+    if (it != container.end())
+        return (true);
+    else
+        return (false);
+}
 
 #endif
--- 08_cpp/ex00/sources/main.cpp
+++ 08_cpp/ex00/sources/main.cpp
@@ -6,13 +6,42 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:44:06 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/19 18:20:22 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/19 18:42:00 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Inc.hpp"
+#include "../includes/Easy.hpp"
+#include <vector>
+#include <list>
 
 int	main(void)
-{   
+{
+    std::vector<int>	v;
+    std::list<int>		l;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+
+    std::cout << "Vector: " << std::endl;
+    std::cout << easyfind(v, 1) << std::endl;
+    std::cout << easyfind(v, 2) << std::endl;
+    std::cout << easyfind(v, 3) << std::endl;
+    std::cout << easyfind(v, 4) << std::endl;
+    std::cout << easyfind(v, 5) << std::endl;
+
+    std::cout << "List: " << std::endl;
+    std::cout << easyfind(l, 1) << std::endl;
+    std::cout << easyfind(l, 2) << std::endl;
+    std::cout << easyfind(l, 3) << std::endl;
+    std::cout << easyfind(l, 4) << std::endl;
+    std::cout << easyfind(l, 5) << std::endl;
+
     return (0);
 }

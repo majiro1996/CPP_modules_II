7b008314cdb60e832e882efbe4ea2e24597494b7
Majiro <majiro1996@gmail.com>
2023-09-26 13:50:29 +0200

    06-00 type

--- 06_cpp/ex00/sources/Converter.cpp
+++ 06_cpp/ex00/sources/Converter.cpp
@@ -6,7 +6,7 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:12:52 by manujime          #+#    #+#             */
-/*   Updated: 2023/09/20 12:15:40 by manujime         ###   ########.fr       */
+/*   Updated: 2023/09/26 13:49:51 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static int myStoi(std::string &str)
 	return (num);
 }
 
-static int  myStof(std::string str)
+static float  myStof(std::string str)
 {
 	float n;
 	std::stringstream ss(str);
@@ -44,7 +44,7 @@ static int  myStof(std::string str)
 	return (n);
 }
 
-static int myStod(std::string str)
+static double myStod(std::string str)
 {
 	double n;
 	std::stringstream ss(str);
@@ -62,16 +62,14 @@ static bool isChar(std::string str)
 
 static bool isInt(std::string str)
 {
-	if (isPseudoFloat(str) || isPseudoDouble(str))
-		return (false);
-	if (str.at(0) == '-' || str.at(0) == '+' || isdigit(str.at(0)))
+	int c = 0;
+	if (str.at(0) == '-' || str.at(0) == '+')
+		c = 1;
+	for (size_t i = c; i < str.length(); i++)
 	{
-		for (size_t i = 1; i < str.length(); i++)
-		{
-			if (!isdigit(str.at(i)))
-				return (false);
-		}
-	}
+		if (!isdigit(str.at(i)))
+			return (false);
+	} 
 	return (true);
 }
 
@@ -80,8 +78,6 @@ static bool isFloat(std::string str)
 	int pCount = 0;
 	int fCount = 0;
 
-	if (isPseudoFloat(str))
-		return (true);
 	if (str.at(str.length() - 1) != 'f')
 		return (false);
 	if (str.at(0) == '-' || str.at(0) == '+' || isdigit(str.at(0)))
@@ -89,92 +85,87 @@ static bool isFloat(std::string str)
 		for (size_t i = 1; i < str.length(); i++)
 		{
 			if (str.at(i) == '.')
+			{
 				pCount++;
+				continue;
+			}
 			if (str.at(i) == 'f')
 				fCount++;
 			else if (!isdigit(str.at(i)))
 				return (false);
 		}
 	}
-	if (pCount != 1 || fCount != 1)
-		return (false);
+	if (pCount > 1 || fCount > 1)
+		return (false); 
 	return (true);
 }
 
 static bool isDouble(std::string str)
 {
-	try
-	{
-		myStod(str);
-	}
-	catch (std::exception &e)
-	{
+	int pCount = 0;
+
+	if (str.at(str.length() - 1) == 'f')
 		return (false);
+	if (str.at(0) == '-' || str.at(0) == '+' || isdigit(str.at(0)))
+	{
+		for (size_t i = 1; i < str.length(); i++)
+		{
+			if (str.at(i) == '.')
+				pCount++;
+			else if (!isdigit(str.at(i)))
+				return (false);
+		}
 	}
+	if (pCount != 1)
+		return (false); 
 	return (true);
 }
 
-static void _convertChar(std::string str)
+static void _convertChar(char c)
 {
-	std::cout << "char: '" << str[0] << "'" << std::endl;
-	char	c = str[0];
-
 	std::cout << "char: '" << c << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(c) << std::endl;
 	std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
 	std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
 }
 
-static void _convertInt(std::string str)
+static void _convertInt(int nbr)
 {
-	std::cout << "int: " << str << std::endl;
-	int		i = myStoi(str);
-
-	if (i < 32 || i > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: '" << static_cast<char>(i) << "'" << std::endl;
-	std::cout << "int: " << i << std::endl;
-	std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
-	std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+	std::cout << "char: '" << static_cast<char>(nbr) << "'" << std::endl;
+	std::cout << "int: " << nbr << std::endl;
+	std::cout << "float: " << static_cast<float>(nbr) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(nbr) << ".0" << std::endl;
 }
 
 static void _converFloatPL(std::string str)
 {
-	std::cout << "float: " << str << std::endl;
-	float	f = myStof(str);
-
 	std::cout << "char: impossible" << std::endl;
 	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: " << f << "f" << std::endl;
-	std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+	std::cout << "float: " << str << std::endl;
+	std::cout << "double: " << str.substr(0, str.length() - 1) << std::endl;
 }
 
-static void _convertFloat(std::string str)
+static void _convertFloat(float f)
 {
-	if (isPseudoFloat(str))
-	{
-		_converFloatPL(str);
-		return ;
-	}
-	
-	std::cout << "float: " << str << std::endl;
-	float	f = myStof(str);
-
 	if (f < 32 || f > 126)
 		std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: '" << static_cast<char>(f) << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(f) << std::endl;
 	std::cout << "float: " << f << "f" << std::endl;
-	std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+	std::cout << "double: " << static_cast<double>(f) << std::endl;
 }
 
 static void _converDobublePL(std::string str)
 {
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << str << "f" << std::endl;
 	std::cout << "double: " << str << std::endl;
-	double	d = myStod(str);
+}
 
+static void _convertDouble(double d)
+{
 	if (d < 32 || d > 126)
 		std::cout << "char: Non displayable" << std::endl;
 	else
@@ -184,43 +175,27 @@ static void _converDobublePL(std::string str)
 	std::cout << "double: " << d << std::endl;
 }
 
-static void _convertDouble(std::string str)
-{
-	if (isPseudoDouble(str))
-	{
-		_converDobublePL(str);
-		return ;
-	}
-	std::cout << "double: " << str << std::endl;
-	double	d = myStod(str);
-
-	if (d < 32 || d > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
-	std::cout << "int: " << static_cast<int>(d) << std::endl;
-	std::cout << "float: " << static_cast<float>(d) << "0.f" << std::endl;
-	std::cout << "double: " << d << ".0" << std::endl;
-}
-
 enum Type
 {
 	_char,
 	_int,
 	_float,
 	_double,
-	_error
+	_error,
+	_pseudo
 };
 
 static Type getType(const std::string str)
 {
+	if (isPseudoFloat(str) || isPseudoDouble(str))
+		return (_pseudo);
 	if (isChar(str))
 		return (_char);
 	if (isInt(str))
 		return (_int);
-	if (isFloat(str) || isPseudoFloat(str))
+	if (isFloat(str))
 		return (_float);
-	if (isDouble(str) || isPseudoDouble(str))
+	if (isDouble(str))
 		return (_double);
 	return (_error);
 }
@@ -230,19 +205,26 @@ void ScalarConverter::convert(std::string str)
 	switch (getType(str))
 	{
 		case _char:
-			_convertChar(str);
-			break;
-		case _int:
-			_convertInt(myStoi(str)); //fix this
+			_convertChar(str[0]);
 			break;
 		case _float:
-			_convertFloat(str);
+			_convertFloat(myStof(str));
 			break;
 		case _double:
-			_convertDouble(str);
+			_convertDouble(myStod(str));
+			break;
+		case _pseudo:
+			if (isPseudoFloat(str))
+				_converFloatPL(str);
+			else
+				_converDobublePL(str);
+			break;
+		case _int:
+			_convertInt(myStoi(str));
 			break;
 		default:
 			std::cout << "Error: invalid input" << std::endl;
 			break;
 	}
+	//std::cout << getType(str) << std::endl;
 }
